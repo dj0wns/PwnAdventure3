c28aabd09e332fd6f0e81ef1d3f39e4dcb4691ea
Derek Jones <derekjones@asu.edu>
2019-08-10 15:49:13 -0400

    Simple parsing updates

--- PwnAdventure3_Proxy_Fork/parser.py
+++ PwnAdventure3_Proxy_Fork/parser.py
@@ -1,2 +1,45 @@
+HIDE_KNOWN = True;
+#HIDE_KNOWN = False;
+
 def parse(data, port, origin):
-  print("{}({}) {}".format(origin, port, data.hex()))
+  #ignore server data
+  if port == 3333:
+    return;
+  
+  #if origin == "server":
+  #  return
+
+  op = data.hex()[:4];
+
+  #noop
+  if op == "0000":
+    return;
+  
+  #location op
+  elif op == "6d76":
+    x = data.hex()[4:12]
+    y = data.hex()[12:20]
+    z = data.hex()[20:28]
+    rotation = data.hex()[28:36]
+    if not HIDE_KNOWN:
+      print("Location OP: {} - {} - {} - {} - {}".format(op,x,y,z,rotation))
+  #weapon firing op
+  elif op == "2a69":
+    weaponID = data.hex()[4:8]
+    unknown = data.hex()[8:68]
+    posx = data.hex()[68:76]
+    posy = data.hex()[76:84]
+    posz = data.hex()[84:92]
+    rot = data.hex()[92:100] 
+    blank = data.hex()[100:108] 
+    if not HIDE_KNOWN:
+      print("Shoot OP: {} - weaponID: {} - position: {} - {} - {} - rotation: {} - Unknown - {} - {}".format(op,weaponID,posx,posy,posz,rot,blank,unknown))
+  #kill projectile op
+  elif op == "7878":
+    projID = data.hex()[4:32]
+    #if not HIDE_KNOWN:
+    print("Projectile dead OP: {} - projectileID: {}".format(op,projID))
+
+  else :
+    #print unparsed ops
+    print("{}({}) {}".format(origin, port, data.hex()))
--- PwnAdventure3_Proxy_Fork/proxy.py
+++ PwnAdventure3_Proxy_Fork/proxy.py
@@ -1,9 +1,11 @@
 import socket
 import os
 import importlib
+import queue
 from threading import Thread
 
 import parser
+import packet
 
 class ProxyToServer(Thread):
 
@@ -12,6 +14,7 @@ class ProxyToServer(Thread):
     self.game = None #game client socket not known yet
     self.port = port
     self.host = host
+    self.sendQueue = queue.Queue()
     #Socket example taken from python docs
     self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.server.connect((host, port))
@@ -29,14 +32,17 @@ class ProxyToServer(Thread):
           print('server[{}]'.format(self.port),e)
         # forward to client
         self.game.sendall(data)
+      if not self.sendQueue.empty():
+        item = self.sendQueue.get()
+        self.server.sendall(item)
 
 class GameToProxy(Thread):
-
   def __init__(self, host, port):
     super(GameToProxy, self).__init__()
     self.server = None # real server socket not known yet
     self.port = port
     self.host = host
+    self.sendQueue = queue.Queue()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((host,port))
@@ -81,14 +87,32 @@ class Proxy(Thread):
       self.p2s.start()
 
 if __name__ == "__main__":
-  master_server = Proxy('0.0.0.0', '127.0.0.1', 3344, 3333)
+  master_server = Proxy('0.0.0.0', '192.168.1.10', 3333, 3333)
   master_server.start()
+  
+
+  #name and keep references to game servers
+  game_server0 = Proxy('0.0.0.0','192.168.1.10',3000,3000)
+  game_server1 = Proxy('0.0.0.0','192.168.1.10',3001,3001)
+  game_server2 = Proxy('0.0.0.0','192.168.1.10',3002,3002)
+  game_server3 = Proxy('0.0.0.0','192.168.1.10',3003,3003)
+  game_server4 = Proxy('0.0.0.0','192.168.1.10',3004,3004)
+  game_server5 = Proxy('0.0.0.0','192.168.1.10',3005,3005)
+  game_server0.start()
+  game_server1.start()
+  game_server2.start()
+  game_server3.start()
+  game_server4.start()
+  game_server5.start()
 
   #execute commands while monitoring is running
   while True:
     try:
       cmd = input('$ ')
+      importlib.reload(packet)
       if cmd[:4] == 'quit':
         os._exit(0)
+      elif cmd[:4] == 'fire':
+        game_server0.p2s.sendQueue.put(packet.GRENADE_PACKET)
     except Exception as e:
       print(e)
